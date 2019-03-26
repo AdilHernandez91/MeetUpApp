5c7a9c327719ff5756374ad94733c6ec311a7a60
German Hernandez <adil.hernandez91@gmail.com>
2019-03-26 18:43:49 +0100

    Initial Commit

---
+++ Meetup App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meetup App
+//
+//  Created by German Hernandez on 26/03/2019.
+//  Copyright © 2019 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
